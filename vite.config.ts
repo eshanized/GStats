3140737d6f9f0fc8f48ca38309b37f5df1f5a515
eshanized <m.eshanized@gmail.com>
2024-12-28 14:55:10 +0530

    fix: the build rollup

--- vite.config.ts
+++ vite.config.ts
@@ -3,20 +3,5 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
-  base: '/gstats/', // Replace with your repository name
-  build: {
-    rollupOptions: {
-      output: {
-        entryFileNames: 'script.js', // Bundle JavaScript as script.js
-        chunkFileNames: '[name].js', // Optional: name other chunks
-        assetFileNames: (assetInfo) => {
-          // Check if the asset name exists and ends with .css
-          if (assetInfo.name && assetInfo.name.endsWith('.css')) {
-            return 'style.css'; // Rename CSS files to style.css
-          }
-          return '[name][extname]'; // Default naming for other assets
-        },
-      },
-    },
-  },
+  base: '/gstats/',
 })
